5c6e0d2a1725f7c5652cb6484a6f283da21c89c3
Dacuy <dacmaspt@gmail.com>
2024-08-24 13:19:58 +0100

    en default

--- config.lua
+++ config.lua
@@ -1,4 +1,4 @@
 Config = {}
-Config.Language = 'es' -- es / en / pt / fr / ru / This are the avaiable languages by now. soon Russian
+Config.Language = 'en' -- es / en / pt / fr / ru / This are the avaiable languages by now. soon Russian
 Config.Server_Name = 'DAC PAUSE MENU' -- WRITE THE NAME WITH SPACES BETWEEN EACH WORD FOR EXAMPLE IF YOUR SERVER NAME IS: YourServer write Your Server. In capital letters will be better.
 Config.Server_Logo = 'https://cdn.discordapp.com/attachments/1236286793406742599/1272202075002114048/gold-lion-logo-template-purple-260nw-1616089000_1.png?ex=66ca98c3&is=66c94743&hm=c07c80d40ff0d5a6b061427ecb9a29993530f1818b72da80a45d9c5b9600645c&' -- URL TO YOUR LOGO FOR EXAMPLE A DISCORD URL
